c745c2ce5dd725c0a164d7ed81f7891ac0a15e65
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-07-16 20:40:00 +0200

    Item, map

--- Code/DirectBitmap.cs
+++ Code/DirectBitmap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 
 namespace DungeonsAndDungeons
--- Code/Item.cs
+++ Code/Item.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DungeonsAndDungeons
+namespace DungeonsAndDungeons.Code
 {
-    class Map
-    {
-    
-    }
+	class Item
+	{
+		public Dictionary<string, object> Attributes { get; set; }
+
+	}
 }
