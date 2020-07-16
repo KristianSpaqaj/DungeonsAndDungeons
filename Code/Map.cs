ca441e963d52b329f565fde2a3bc79cd16e05221
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-07-16 21:08:23 +0200

    map or some street shit

--- Code/Map.cs
+++ Code/Map.cs
@@ -8,6 +8,12 @@ namespace DungeonsAndDungeons
 {
     class Map
     {
-    
+        public List<int> Tiles { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        // Tilføj Textures
+
+        //Tilføj GetTexture
     }
 }
