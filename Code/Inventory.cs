07f39412a39e82a36a715b1dd6828765415b1129
gstok123 <gstok123@gmail.com>
2020-07-16 20:06:19 +0200

    implemented invetory class

--- Code/Inventory.cs
+++ Code/Inventory.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace DungeonsAndDungeons
 {
-    class Inventory
+    class Inventory<T>
     {
+        private List<T> Items { get; set; }
+        public int ItemLimit { get; set; }
+        
+        public void AddItem(T item)
+        {
+            Items.Add(item);
+        }
+        
+        public T GetItem(int index)
+        {
+            return Items.ElementAt(index);
+        }
+
+        public List<T> GetAllItems()
+        {
+            return Items;
+        }
+
+        public void DropItem(int index)
+        {
+            Items.RemoveAt(index);
+        }
+
     }
 }
