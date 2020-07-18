6a757dc1ad2ba34aa5324bcd2098f51be7b0df71
gstok123 <gstok123@gmail.com>
2020-07-18 14:55:23 +0200

    Added comment

--- Code/TurnProcessor.cs
+++ Code/TurnProcessor.cs
@@ -9,10 +9,10 @@ namespace DungeonsAndDungeons
     class TurnProcessor
     {
         public Entity CurrentEntity { get; set; }
-        
 
         public void RunCurrentTurn(Level currentLevel)
         {
+            //Get entities with Attribute Behaviour(Behaviour object)
             //CurrentEntity.GetTurnCommand()
             //Command.execute(currentLevel)
             throw new NotImplementedException();
