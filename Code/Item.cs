5b1ccc1733cfdf58f539ac161bd74ab6cc44b218
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-07-28 17:08:47 +0200

    Weapon nedarver fra Item

--- Code/Item.cs
+++ Code/Item.cs
@@ -5,5 +5,12 @@ namespace DungeonsAndDungeons
     public class Item
     {
         public Dictionary<string, object> Attributes { get; set; }
+        public Sprite Sprite { get; set; }
+
+        public Item(Dictionary<string, object> attributes, Sprite sprite)
+        {
+            Attributes = attributes;
+            Sprite = sprite;
+        }
     }
 }
--- Code/Weapon.cs
+++ Code/Weapon.cs
@@ -2,10 +2,17 @@
 
 namespace DungeonsAndDungeons
 {
-    public class Weapon
+    public class Weapon : Item
     {
         public double Damage { get; set; }
         public int Range { get; set; }
-        public Dictionary<string, double> Attributes { get; set; }
+
+        public Weapon(Dictionary<string, object> attributes, Sprite sprite, double damage, int range) : base(attributes, sprite)
+        {
+            Damage = damage;
+            Range = range;
+        }
+
+
     }
 }
