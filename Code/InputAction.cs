33b448cf6f95007cb002eb66e0dbb80ceb129df8
gstok123 <gstok123@gmail.com>
2020-08-06 10:11:43 +0200

    Added inputaction class

---
+++ Code/InputAction.cs
@@ -0,0 +1,16 @@
+﻿namespace DungeonsAndDungeons
+{
+    public class InputAction
+    {
+        public string Action { get; }
+        public double TimeOut { get; }
+        public bool IsRepeatable { get; }
+
+        public InputAction(string action, double timeOut, bool isRepeatable)
+        {
+            Action = action;
+            TimeOut = timeOut;
+            IsRepeatable = isRepeatable;
+        }
+    }
+}
